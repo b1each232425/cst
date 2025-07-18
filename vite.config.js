5f513142b58d71f6e92d5d5c05a205a5652df06b
Xavier Wang <xwcoder7@gmail.com>
2025-07-19 00:05:03 +0800

    增加全局CSS和layout.svelte

--- vite.config.js
+++ vite.config.js
@@ -65,9 +65,9 @@ export default defineConfig({
 		port: 6443,
 		host:'0.0.0.0',
 		proxy:{
-			'/api': "http://localhost:6610",
+			'/api': "http://localhost:6616",
 			"/api/ws":{
-				target: "ws://localhost:6610",
+				target: "ws://localhost:6616",
 				ws: true
 			}
 		},
