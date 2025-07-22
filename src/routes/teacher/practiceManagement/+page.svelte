18f790ef7dcba0e76b20a8ee8ac8709647672d15
lly <3102128343@qq.com>
2025-07-22 23:03:22 +0800

    保存本地修改

---
+++ src/routes/teacher/practiceManagement/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    import Title from "./_components/Title.svelte";
+
+
+</script>
+
+
+<div class="practice-management">
+    <!-- <Title title="练习管理" /> -->
+    <h1>练习管理内容</h1> <!-- ⬅️ 添加这一行 -->
+
+
+</div>
+
+<style>
+
+</style>
