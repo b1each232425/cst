997ac65be6099a37dab17d35c0f2cdced259fd1d
SquareCatFirst <60251940+SquareCatFirst@users.noreply.github.com>
2025-07-30 22:35:08 +0800

    完成考试作答对接 https://git.w2w.me:6443/knowledge-training/exam/-/issues/29

---
+++ src/routes/student/answer/exam/+layout@.svelte
@@ -0,0 +1,18 @@
+
+  <!-- /*
+   * @Author: PENG HAIFENG 1614818457@qq.com
+   * @Date: 2025-04-14 10:55:37
+ * @LastEditors: PENG HAIFENG 1614818457@qq.com
+ * @LastEditTime: 2025-05-28 23:59:56
+   * @FilePath: /tutorial-platform-fe/src/routes/student/studentAnswerExam/+layout.svelte
+   * @Description: 考试练习作答布局
+   */ -->
+<script>
+  // 使用$props()获取子组件
+  let { children } = $props();
+</script>
+
+<div>
+  <!-- 渲染子组件内容 -->
+  {@render children()}
+</div>
