6e5516751bc505ec971a251c7226a3f22c42f00a
bleach <142779648+b1each232425@users.noreply.github.com>
2025-07-30 17:10:35 +0800

    解决build失败

---
+++ src/routes/teacher/exam/_components/RequiredLabel.svelte
@@ -0,0 +1,39 @@
+<script>
+    let { text = "", Asterisk = true, colon = true } = $props();
+</script>
+
+<div class="labelContainer">
+    <span class="labelText">
+        {#if Asterisk}
+            <span class="requiredIcon"> *</span>
+        {/if}
+        {text}
+    </span>
+    <span class="colon">
+        {#if colon}
+            :
+        {/if}
+    </span>
+</div>
+
+<style scoped>
+    .labelContainer {
+        display: flex;
+        align-items: center;
+        font-size: 14px;
+        width: 120px;
+    }
+
+    .labelText {
+        white-space: wrap;
+        overflow: hidden;
+        text-overflow: clip;
+        text-align: right;
+        flex: 1;
+    }
+
+    .requiredIcon {
+        margin-right: 2px;
+        color: var(--red);
+    }
+</style>
