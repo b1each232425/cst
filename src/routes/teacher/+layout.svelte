5fb073263775fe9fe3e9a4163c73d57307e9df83
lanxiong <1062051028@qq.com>
2025-08-02 15:50:24 +0800

    完善布局结构，新增switch、loading测试代码 https://git.w2w.me:6443/knowledge-training/exam/-/issues/12

--- src/routes/teacher/+layout.svelte
+++ src/routes/teacher/+layout.svelte
@@ -29,7 +29,7 @@
   </main>
 </div>
 
-<style lang="scss" scoped>
+<style lang="scss">
   .app {
     display: flex;
     position: fixed;
@@ -37,6 +37,7 @@
     left: 0;
     width: 100%;
     height: 100vh;
+    overflow: hidden;
 
     .sidebar-container {
       top: 0;
@@ -52,46 +53,32 @@
       flex-direction: column;
       width: 100%;
       height: 100%;
-      overflow: hidden;
-      transition: margin-left 0.2s linear;
       margin-left: 235px;
+      transition: margin-left 0.2s linear;
 
       &.shrink {
         margin-left: 0;
       }
 
       header {
-        display: flex;
-        flex-direction: row;
         position: relative;
-        top: 0;
-        left: 0;
-        width: 100%;
-        height: max-content;
+        flex-shrink: 0; /* 禁止压缩 */
+        height: 50px;
         background-color: #f5f5f5;
         z-index: 1001;
       }
 
       .content-container {
-        display: flex;
-        flex-direction: column;
-        position: relative;
-        width: 100%;
-        height: 100%;
+        flex: 1;
+        overflow-y: auto;
         background-color: var(--bg-primary);
-        box-sizing: border-box;
         padding: 30px;
       }
 
       footer {
-        display: flex;
-        position: relative;
-        bottom: 0;
-        left: 0;
-        width: 100%;
-        height: 52px;
+        flex-shrink: 0;
+        height: 50px;
         background-color: #f5f5f5;
-        text-align: center;
       }
     }
   }
